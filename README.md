d36fef9c5fbb696a0be5400007db87d53aad7ffd
samuil atanasov <samuil.atanasov2010@gmail.com>
2025-05-11 22:10:53 +0300

    fix ports

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 Yachts Project DEVELOPMENT
 
 Useful links:
+<br/>
 https://www.w3schools.com/howto/howto_css_navbar_icon.asp
+<br/>
 https://www.w3schools.com/howto/howto_css_fixed_footer.asp
+<br/>
 https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp
+<br/>
 https://www.w3schools.com/jsref/prop_style_display.asp
+<br/>
--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,44 @@
 function showOrHideDiv(divID) {
+
+  //отваряме или затваряме избраното пристанище
   var x = document.getElementById(divID);
   if (x.style.display === "none" || x.style.display === "") {
     x.style.display = "block";
   } else {
     x.style.display = "none";
+  }
+
+  //затваряме останалите пристанища
+  switch(divID) {
+    case 'port-marina-dinevi':
+      document.getElementById('port-pomorie').style.display = "none";
+      document.getElementById('port-nesebyr').style.display = "none";
+      document.getElementById('port-burgas').style.display = "none";
+      document.getElementById('port-sozopol').style.display = "none";
+      break;
+    case 'port-pomorie':
+      document.getElementById('port-marina-dinevi').style.display = "none";
+      document.getElementById('port-nesebyr').style.display = "none";
+      document.getElementById('port-burgas').style.display = "none";
+      document.getElementById('port-sozopol').style.display = "none";
+      break;
+    case 'port-nesebyr':
+      document.getElementById('port-marina-dinevi').style.display = "none";
+      document.getElementById('port-pomorie').style.display = "none";
+      document.getElementById('port-burgas').style.display = "none";
+      document.getElementById('port-sozopol').style.display = "none";
+      break;
+    case 'port-burgas':
+      document.getElementById('port-marina-dinevi').style.display = "none";
+      document.getElementById('port-nesebyr').style.display = "none";
+      document.getElementById('port-pomorie').style.display = "none";
+      document.getElementById('port-sozopol').style.display = "none";
+      break;
+    case 'port-sozopol':
+      document.getElementById('port-marina-dinevi').style.display = "none";
+      document.getElementById('port-nesebyr').style.display = "none";
+      document.getElementById('port-burgas').style.display = "none";
+      document.getElementById('port-pomorie').style.display = "none";
+      break;
   }
 } 
