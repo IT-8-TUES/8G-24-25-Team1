94ba96e01ab3f39517ba9951a7887fc3f36e0fbd
samuil atanasov <samuil.atanasov2010@gmail.com>
2025-05-13 21:25:37 +0300

    index.html content

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 function showOrHideDiv(divID) {
 
   //отваряме или затваряме избраното пристанище
-  var x = document.getElementById(divID);
+  let x = document.getElementById(divID);
   if (x.style.display === "none" || x.style.display === "") {
     x.style.display = "block";
   } else {
@@ -15,30 +15,90 @@ function showOrHideDiv(divID) {
       document.getElementById('port-nesebyr').style.display = "none";
       document.getElementById('port-burgas').style.display = "none";
       document.getElementById('port-sozopol').style.display = "none";
+      document.getElementById('port-duni').style.display = "none";
+      document.getElementById('port-perla').style.display = "none";
+      document.getElementById('port-primorsko').style.display = "none";
+      document.getElementById('port-kiten').style.display = "none";
       break;
     case 'port-pomorie':
       document.getElementById('port-marina-dinevi').style.display = "none";
       document.getElementById('port-nesebyr').style.display = "none";
       document.getElementById('port-burgas').style.display = "none";
       document.getElementById('port-sozopol').style.display = "none";
+      document.getElementById('port-duni').style.display = "none";
+      document.getElementById('port-perla').style.display = "none";
+      document.getElementById('port-primorsko').style.display = "none";
+      document.getElementById('port-kiten').style.display = "none";
       break;
     case 'port-nesebyr':
       document.getElementById('port-marina-dinevi').style.display = "none";
       document.getElementById('port-pomorie').style.display = "none";
       document.getElementById('port-burgas').style.display = "none";
       document.getElementById('port-sozopol').style.display = "none";
+      document.getElementById('port-duni').style.display = "none";
+      document.getElementById('port-perla').style.display = "none";
+      document.getElementById('port-primorsko').style.display = "none";
+      document.getElementById('port-kiten').style.display = "none";
       break;
     case 'port-burgas':
       document.getElementById('port-marina-dinevi').style.display = "none";
       document.getElementById('port-nesebyr').style.display = "none";
       document.getElementById('port-pomorie').style.display = "none";
       document.getElementById('port-sozopol').style.display = "none";
+      document.getElementById('port-duni').style.display = "none";
+      document.getElementById('port-perla').style.display = "none";
+      document.getElementById('port-primorsko').style.display = "none";
+      document.getElementById('port-kiten').style.display = "none";
       break;
     case 'port-sozopol':
       document.getElementById('port-marina-dinevi').style.display = "none";
       document.getElementById('port-nesebyr').style.display = "none";
       document.getElementById('port-burgas').style.display = "none";
       document.getElementById('port-pomorie').style.display = "none";
+      document.getElementById('port-duni').style.display = "none";
+      document.getElementById('port-perla').style.display = "none";
+      document.getElementById('port-primorsko').style.display = "none";
+      document.getElementById('port-kiten').style.display = "none";
+      break;
+    case 'port-duni':
+      document.getElementById('port-marina-dinevi').style.display = "none";
+      document.getElementById('port-nesebyr').style.display = "none";
+      document.getElementById('port-pomorie').style.display = "none";
+      document.getElementById('port-burgas').style.display = "none";
+      document.getElementById('port-sozopol').style.display = "none";
+      document.getElementById('port-perla').style.display = "none";
+      document.getElementById('port-primorsko').style.display = "none";
+      document.getElementById('port-kiten').style.display = "none";
+      break;
+    case 'port-perla':
+      document.getElementById('port-marina-dinevi').style.display = "none";
+      document.getElementById('port-nesebyr').style.display = "none";
+      document.getElementById('port-burgas').style.display = "none";
+      document.getElementById('port-pomorie').style.display = "none";
+      document.getElementById('port-sozopol').style.display = "none";
+      document.getElementById('port-duni').style.display = "none";
+      document.getElementById('port-primorsko').style.display = "none";
+      document.getElementById('port-kiten').style.display = "none";
+      break;
+    case 'port-primorsko':
+      document.getElementById('port-marina-dinevi').style.display = "none";
+      document.getElementById('port-nesebyr').style.display = "none";
+      document.getElementById('port-pomorie').style.display = "none";
+      document.getElementById('port-burgas').style.display = "none";
+      document.getElementById('port-sozopol').style.display = "none";
+      document.getElementById('port-perla').style.display = "none";
+      document.getElementById('port-duni').style.display = "none";
+      document.getElementById('port-kiten').style.display = "none";
+      break;
+    case 'port-kiten':
+      document.getElementById('port-marina-dinevi').style.display = "none";
+      document.getElementById('port-nesebyr').style.display = "none";
+      document.getElementById('port-pomorie').style.display = "none";
+      document.getElementById('port-burgas').style.display = "none";
+      document.getElementById('port-sozopol').style.display = "none";
+      document.getElementById('port-perla').style.display = "none";
+      document.getElementById('port-duni').style.display = "none";
+      document.getElementById('port-primorsko').style.display = "none";
       break;
   }
 } 
